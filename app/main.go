3589b9c0217be6112b738dc7814fc2946b2b62a1
Lewis Mclean <lewismclean21@googlemail.com>
2026-05-12 16:23:10 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -9,6 +9,7 @@ import (
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
+pathFiles := []string(os.ReadDir(PATH))
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
@@ -29,6 +30,13 @@ func main() {
 			if command[5:] == "echo" || command[5:] == "type" || command[5:] == "exit" {
 				fmt.Println(command[5:] + " is a shell builtin")
 			} else {
+				for _, file := range pathFiles {
+					if file.Name() == command[5:] && file.type().IsExec() {
+						println(command[5:] + " is" + file.Path())
+						break
+					}
+				}
+						
 				fmt.Println(command[5:] + ": not found")
 			}
 		} else {
