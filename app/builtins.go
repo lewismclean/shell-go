83ccdc9f142d5497f77a35f6c26cb78ebe4efd52
Lewis Mclean <lewismclean21@googlemail.com>
2026-05-18 20:29:27 +0100

    codecrafters submit [skip ci]

---
+++ app/builtins.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func runEcho(arg string) {
+	fmt.Println(arg)
+}
+
+func runType(arg string) {
+	if arg == "echo" || arg == "type" || arg == "exit" {
+		fmt.Println(arg + " is a shell builtin")
+	} else {
+		path, found := findInPath(arg)
+		if found {
+			fmt.Println(arg + " is " + path)
+		} else {
+			fmt.Println(arg + ": not found")
+		}
+	}
+}
+
+func runExit() {
+	os.Exit(0)
+}
--- app/main.go
+++ app/main.go
@@ -19,20 +19,11 @@ func main() {
 		}
 		command = strings.TrimSpace(command)
 		if command == "exit" {
-			break
+			runExit()
 		} else if strings.HasPrefix(command, "echo ") {
-			fmt.Println(command[5:])
+			runEcho(command[5:])
 		} else if strings.HasPrefix(command, "type ") {
-			if command[5:] == "echo" || command[5:] == "type" || command[5:] == "exit" {
-				fmt.Println(command[5:] + " is a shell builtin")
-			} else {
-				path, found := findInPath(command[5:])
-				if found {
-					fmt.Println(command[5:] + " is " + path)
-				} else {
-					fmt.Println(command[5:] + ": not found")
-				}
-			}
+			runType(command[5:])
 		} else {
 			fmt.Println(command + ": command not found")
 		}
