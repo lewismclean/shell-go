53fdd22f2e4983923cc6a98eecd9e8475e6511c1
Lewis Mclean <lewismclean21@googlemail.com>
2026-05-19 20:24:16 +0100

    codecrafters submit [skip ci]

--- app/execute.go
+++ app/execute.go
@@ -7,7 +7,7 @@ import (
 
 func runExecutable(name string, path string, args []string) {
 	cmd := exec.Command(path, args...)
-	cmd.Args := append([]string{name} args...)
+	cmd.Args := append([]string{name}, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
